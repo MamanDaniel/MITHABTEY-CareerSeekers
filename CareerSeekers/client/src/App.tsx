6d7a80cd1c37d6a3300d98c4a2a56d291badbc5b
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-05-28 15:53:22 +0300

    install tailwind vite

--- CareerSeekers/client/src/App.tsx
+++ CareerSeekers/client/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
      
