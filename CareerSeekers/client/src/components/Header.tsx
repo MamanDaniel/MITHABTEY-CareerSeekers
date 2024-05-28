0441f2ab72f5dff24c091980fbf7aaabc2c5f20a
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-05-28 17:13:35 +0300

    add heder for example

--- CareerSeekers/client/src/components/Header.tsx
+++ CareerSeekers/client/src/components/Header.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default function Header() {
-  return (
-    <div>Header</div>
+    return (
+    // css the header to be with grey background and text to be white
+    // the nav links should be inline and the text should be red
+
+    
+    <header className='bg-gray-300 ray- text-white'>
+        <nav className='inline'>
+            <a href='/home' className='text-red-700 font-bold  uppercase text-center p-4 m-4 '>Home</a>
+            <a href='/about' className='text-red-700 font-bold  uppercase text-center p-4 m-4 '>About</a>
+            </nav>
+    </header>
+    
   )
 }
--- CareerSeekers/client/src/pages/Home.tsx
+++ CareerSeekers/client/src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
     const [count, setCount] = useState(0)
   return (
       <>
-          <div className='font-bold text-green-400'>Home</div>
+          <div className='font-bold text-green-400'>Home page</div>
       </> 
   )
 }
