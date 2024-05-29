28e10372c7dd3eab284df9dceffe26c5793ff272
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-05-29 17:49:17 +0300

    update header

--- CareerSeekers/client/src/components/Header.tsx
+++ CareerSeekers/client/src/components/Header.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
-    return (
-    // css the header to be with grey background and text to be white
-    // the nav links should be inline and the text should be red
+  return (
 
-    
-    <header className='bg-gray-300 ray- text-white'>
-        <nav className='inline'>
-            <a href='/home' className='text-red-700 font-bold  uppercase text-center p-4 m-4 '>Home</a>
-            <a href='/about' className='text-red-700 font-bold  uppercase text-center p-4 m-4 '>About</a>
-            </nav>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx auto p-3'>
+        <Link to='home'>
+          <h1 className='font-bold text-sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Career</span>
+            <span className='text-slate-700'>Seekers</span>
+          </h1>
+        </Link>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 ' />
+          <FaSearch className='text-slate-600' />
+        </form>
+        <ul className=' flex gap-4'>
+          <Link to='home'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+          </Link>
+          <Link to='about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+          </Link>
+        </ul>
+      </div>
     </header>
-    
   )
 }
