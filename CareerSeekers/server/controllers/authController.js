2dd7530e9f16a8d77af07cd703a37b54c3c9de58
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-05-30 16:17:10 +0300

    Hansle eror and add user

--- CareerSeekers/server/controllers/authController.js
+++ CareerSeekers/server/controllers/authController.js
@@ -1,4 +1,14 @@
-export const signup = (req, res) => {
-    console.log('signup request received');
-    console.log(req.body);
+import User from "../models/userModel.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 12);
+    const user = new User({ username, email, password: hashedPassword});
+    try {
+        await user.save();
+        res.status(201).json('User created successfully!');
+    }catch(eror){
+        res.status(500).json(eror.message);
+    }
 };
