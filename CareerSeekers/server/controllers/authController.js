84824cf11c163332eb1721c9fd99e9584e2e51b2
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-05-30 16:27:08 +0300

    add error handelr

--- CareerSeekers/server/controllers/authController.js
+++ CareerSeekers/server/controllers/authController.js
@@ -1,14 +1,14 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 12);
     const user = new User({ username, email, password: hashedPassword});
     try {
         await user.save();
         res.status(201).json('User created successfully!');
-    }catch(eror){
-        res.status(500).json(eror.message);
+    }catch(err){
+        next(err);
     }
 };
