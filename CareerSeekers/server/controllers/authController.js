e45a2f1273177c66d18ad1983600a60098e16a2e
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-06-01 09:39:09 +0300

    Update jwt

--- CareerSeekers/server/controllers/authController.js
+++ CareerSeekers/server/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -8,7 +10,25 @@ export const signup = async (req, res, next) => {
     try {
         await user.save();
         res.status(201).json('User created successfully!');
-    }catch(err){
-        next(err);
+    }catch(error){
+        next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if(!validUser){
+            return next(errorHandler('User not found', 404));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler('Wrong email or password', 401));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json({ validUser});
+    } catch (error) {
+        next(error);
+    }
+}
