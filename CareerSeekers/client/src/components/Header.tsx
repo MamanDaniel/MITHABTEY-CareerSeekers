abdcf9ddb720fe25287bb86ac4b6c3fa3dd27a5f
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-06-02 15:43:03 +0300

    uodate redux

--- CareerSeekers/client/src/components/Header.tsx
+++ CareerSeekers/client/src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
 
     <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx auto p-3'>
+      <div className='flex justify-between items-center max-w-8xl mx auto p-3'>
         <Link to='home'>
           <h1 className='font-bold text-sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Career</span>
