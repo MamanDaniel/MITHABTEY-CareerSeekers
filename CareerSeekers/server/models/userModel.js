4fdec6f3235b024972397151854bbeaf077c7e92
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-06-15 17:27:59 +0300

    add server updateUserTraits working api also update user model

--- CareerSeekers/server/models/userModel.js
+++ CareerSeekers/server/models/userModel.js
@@ -18,10 +18,39 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
-    role: {
-        type: String,
-        enum: ['Regular', 'Admin'],
-        default: 'Regular',
+    traits: {
+        Business: {
+            type: Number,
+            default: 0
+        },
+        GeneralCulture: {
+            type: Number,
+            default: 0
+        },
+        ArtsAndEntertainment: {
+            type: Number,
+            default: 0
+        },
+        Science: {
+            type: Number,
+            default: 0
+        },
+        Organization: {
+            type: Number,
+            default: 0
+        },
+        Service: {
+            type: Number,
+            default: 0
+        },
+        Outdoor: {
+            type: Number,
+            default: 0
+        },
+        Technology: {
+            type: Number,
+            default: 0
+        }
     },
 },
     { timestamps: true }
