b0bcea754a1c9a60a3996026e2cd93d1ac6fd33a
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-06-16 10:12:44 +0300

    update names in PrivateRouth

--- CareerSeekers/client/src/App.tsx
+++ CareerSeekers/client/src/App.tsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Header from './components/Header'
 import Signin from './pages/Signin'
-import { PrivateRouth, PrivateRouteLogdIn } from './components/PrivateRouth'
+import { PrivateRouteNotLoggedIn, PrivateRouteLoggedIn } from './components/PrivateRouth'
 import Profile from './pages/Profile'
 import RamakQuestionnaire from './pages/RamakQuestionnaire'
 
@@ -14,12 +14,12 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         {/* Private route - all the route inside this section, the user cant get access while he logedin */}
-        <Route element={<PrivateRouteLogdIn />}>
+        <Route element={<PrivateRouteLoggedIn />}>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
         </Route>
         {/* Private route - all the route inside this section require log in */}
-        <Route element={<PrivateRouth />}>
+        <Route element={<PrivateRouteNotLoggedIn />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/RamakQuestionnaire' element={<RamakQuestionnaire />} />
         </Route>
--- CareerSeekers/client/src/components/PrivateRouth.tsx
+++ CareerSeekers/client/src/components/PrivateRouth.tsx
@@ -2,13 +2,13 @@ import { useSelector } from "react-redux"
 import { Outlet, Navigate } from "react-router-dom"
 
 // if user is logged in, render the Outlet component, otherwise redirect to the Signin page
-export  function PrivateRouth() {
+export  function PrivateRouteNotLoggedIn() {
   const { currentUser } = useSelector((state: any) => state.user)
     return currentUser ? <Outlet /> : <Navigate to='/signin' />; 
 }
 
 // if user is logged in, render the Outlet component, otherwise redirect to the Home page
-export function PrivateRouteLogdIn() {
+export function PrivateRouteLoggedIn() {
   const { currentUser } = useSelector((state: any) => state.user)
   return currentUser ?  <Navigate to='/home' />: <Outlet /> ; 
 }
