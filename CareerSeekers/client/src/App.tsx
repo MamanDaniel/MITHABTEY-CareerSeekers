b6ad82382e5a997917cde8548b90f76e2678baec
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-06-16 14:03:51 +0300

    update Home page

--- CareerSeekers/client/src/App.tsx
+++ CareerSeekers/client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/newHome'
+import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Header from './components/Header'
 import Signin from './pages/Signin'
