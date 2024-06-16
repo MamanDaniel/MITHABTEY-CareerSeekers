4950c108e5e79ab874209385e87eed8a06746ea7
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-06-16 10:05:38 +0300

    update route while logged in - cant get acess to signin/up

--- CareerSeekers/client/src/components/PrivateRouth.tsx
+++ CareerSeekers/client/src/components/PrivateRouth.tsx
@@ -1,9 +1,14 @@
 import { useSelector } from "react-redux"
 import { Outlet, Navigate } from "react-router-dom"
 
-export default function PrivateRouth() {
+// if user is logged in, render the Outlet component, otherwise redirect to the Signin page
+export  function PrivateRouth() {
   const { currentUser } = useSelector((state: any) => state.user)
-    return currentUser ? <Outlet /> : <Navigate to='/signin' />;
-  
-  
+    return currentUser ? <Outlet /> : <Navigate to='/signin' />; 
+}
+
+// if user is logged in, render the Outlet component, otherwise redirect to the Home page
+export function PrivateRouteLogdIn() {
+  const { currentUser } = useSelector((state: any) => state.user)
+  return currentUser ?  <Navigate to='/home' />: <Outlet /> ; 
 }
