673d9c3960ff3c8b5d4116c1741eab795d3ae47a
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-06-16 12:32:33 +0300

    working user sign in / up

--- CareerSeekers/server/controllers/authController.js
+++ CareerSeekers/server/controllers/authController.js
@@ -6,11 +6,15 @@ import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 12);
-    const user = new User({ username, email, password: hashedPassword});
+    const user = new User({ username, email, password: hashedPassword });
     try {
         await user.save();
         res.status(201).json('User created successfully!');
-    }catch(error){
+    } catch (error) {
+        // if response conatain 'E11000 duplicate key error' return email already exists
+        if (error.message.includes('E11000 duplicate key error')) {
+            return next(errorHandler(400, 'Email already exists!'));
+        }
         next(error);
     }
 };
@@ -19,11 +23,11 @@ export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if(!validUser){
+        if (!validUser) {
             return next(errorHandler(404, 'User not found'));
         }
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword){
+        if (!validPassword) {
             return next(errorHandler(401, 'Wrong email or password'));
         }
         // create a token for the user based on the user id in the database
@@ -31,16 +35,17 @@ export const signin = async (req, res, next) => {
         // remove password from the user object
         const { password: pass, ...rest } = validUser._doc;
         // send the token in a cookie
-        res.cookie('access_token', token, { httpOnly: true }).status(200).json( rest );
-    
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+
     } catch (error) {
         next(error);
     }
 }
 
 export const signout = (req, res, next) => {
-try {
-    res.clearCookie('access_token').json('User signed out successfully!');
-} catch (error) {
-    next(error);
-}}
+    try {
+        res.clearCookie('access_token').json('User signed out successfully!');
+    } catch (error) {
+        next(error);
+    }
+}
