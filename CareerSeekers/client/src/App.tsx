7d1c74e5db8b35a1e10c6a2305a6db5131aa0baa
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-07-06 15:24:48 +0300

    add job page

--- CareerSeekers/client/src/App.tsx
+++ CareerSeekers/client/src/App.tsx
@@ -9,6 +9,7 @@ import Profile from './pages/Profile'
 import RamakQuestionnaire from './pages/RamakQuestionnaire'
 import AdminPanel from './pages/AdminPanel'
 import AddJob from './pages/AddJob'
+import Jobs from './pages/Jobs'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route element={<PrivateRouteNotLoggedIn />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/RamakQuestionnaire' element={<RamakQuestionnaire />} />
+          <Route path='/jobs' element={<Jobs />} />
           <Route element={<ValidateAdmin />}>
             <Route path='/adminpanel' element={<AdminPanel />} />
             <Route path='/adminpanel/addjob' element={<AddJob />} />
