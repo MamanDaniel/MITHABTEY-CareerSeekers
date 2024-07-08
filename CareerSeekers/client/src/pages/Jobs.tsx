791ca522e1f272824416524292a9378143596d41
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-07-08 16:00:02 +0300

    working selectbox

--- CareerSeekers/client/src/pages/Jobs.tsx
+++ CareerSeekers/client/src/pages/Jobs.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import JobsFieldCountChart from '../components/jobs/JobsFieldCountChart';
-import PrerequisitesChart from '../components/jobs/PrerequisitesChart';
 import SalaryChart from '../components/jobs/SalaryChart';
 import JobTable from '../components/jobs/JobTable';
+import PrerequisitesChart from '../components/jobs/PrerequisitesChart';
+import Select from 'react-select';
 
 const Jobs: React.FC = () => {
     const [loading, setLoading] = useState(true);
@@ -11,10 +12,48 @@ const Jobs: React.FC = () => {
     const [search, setSearch] = useState("");
     const [filteredData, setFilteredData] = useState(data.data);
     const [jobFieldChartData, setJobFieldChartData] = useState<{ labels: string[], counts: number[] }>({ labels: [], counts: [] });
-    const [prerequisitesChartData, setPrerequisitesChartData] = useState<{ labels: string[], counts: number[] }>({ labels: [], counts: [] });
     const [salaryChartData, setSalaryChartData] = useState<{ labels: string[], counts: number[] }>({ labels: [], counts: [] });
+    const [selectedJobFields, setSelectedJobFields] = useState<{ value: string, label: string }[]>([]);
     const [selectedJob, setSelectedJob] = useState<{ [key: string]: number }>({});
     const [showPrerequisites, setShowPrerequisites] = useState(false);
+    const [options, setOptions] = useState<{ value: string, label: string }[]>([]);
+    // Function to aggregate data by job field
+    const aggregateDataByJobField = (jobs: { joblField: string }[]) => {
+        const aggregatedData: { [key: string]: number } = {};
+
+        jobs.forEach(job => {
+            const jobField = job.joblField;
+            if (aggregatedData[jobField]) {
+                aggregatedData[jobField] += 1;
+            } else {
+                aggregatedData[jobField] = 1;
+            }
+        });
+
+        return aggregatedData;
+    };
+
+    // Function to aggregate average salary by job field
+    const aggregateAverageSalaryByJobField = (jobs: { joblField: string, AverageSalary: number }[]) => {
+        const salaryData: { [key: string]: { totalSalary: number, count: number } } = {};
+
+        jobs.forEach(job => {
+            const jobField = job.joblField;
+            if (salaryData[jobField]) {
+                salaryData[jobField].totalSalary += job.AverageSalary;
+                salaryData[jobField].count += 1;
+            } else {
+                salaryData[jobField] = { totalSalary: job.AverageSalary, count: 1 };
+            }
+        });
+
+        const averageSalaryData: { [key: string]: number } = {};
+        for (const field in salaryData) {
+            averageSalaryData[field] = salaryData[field].totalSalary / salaryData[field].count;
+        }
+
+        return averageSalaryData;
+    };
 
     useEffect(() => {
         const fetchAllJobs = async () => {
@@ -31,67 +70,14 @@ const Jobs: React.FC = () => {
                 setError('Failed to fetch jobs');
                 setLoading(false);
             }
-        }
+        };
         fetchAllJobs();
     }, []);
 
     useEffect(() => {
-        const aggregateDataByJobField = (jobs: { joblField: string }[]) => {
-            const aggregatedData: { [key: string]: number } = {};
-
-            jobs.forEach(job => {
-                const jobField = job.joblField;
-                if (aggregatedData[jobField]) {
-                    aggregatedData[jobField] += 1;
-                } else {
-                    aggregatedData[jobField] = 1;
-                }
-            });
-
-            return aggregatedData;
-        };
-
-        const aggregateAverageSalaryByJobField = (jobs: { joblField: string, AverageSalary: number }[]) => {
-            const salaryData: { [key: string]: { totalSalary: number, count: number } } = {};
-
-            jobs.forEach(job => {
-                const jobField = job.joblField;
-                if (salaryData[jobField]) {
-                    salaryData[jobField].totalSalary += job.AverageSalary;
-                    salaryData[jobField].count += 1;
-                } else {
-                    salaryData[jobField] = { totalSalary: job.AverageSalary, count: 1 };
-                }
-            });
-
-            const averageSalaryData: { [key: string]: number } = {};
-            for (const field in salaryData) {
-                averageSalaryData[field] = salaryData[field].totalSalary / salaryData[field].count;
-            }
-
-            return averageSalaryData;
-        };
-
-        const aggregatePrerequisites = (jobs: { Prerequisites: { [key: string]: number } }[]) => {
-            const prerequisitesData: { [key: string]: number } = {};
-
-            jobs.forEach(job => {
-                const prerequisites = job.Prerequisites;
-                for (const key in prerequisites) {
-                    if (prerequisitesData[key]) {
-                        prerequisitesData[key] += prerequisites[key];
-                    } else {
-                        prerequisitesData[key] = prerequisites[key];
-                    }
-                }
-            });
-
-            return prerequisitesData;
-        };
-
         const aggregatedJobFieldData = aggregateDataByJobField(data.data);
         const aggregatedAverageSalaryData = aggregateAverageSalaryByJobField(data.data);
-        const aggregatedPrerequisitesData = aggregatePrerequisites(data.data);
+        setOptions(data.data.map(job => job.joblField).filter((value, index, self) => self.indexOf(value) === index).map(label => ({ value: label, label })));
 
         setJobFieldChartData({
             labels: Object.keys(aggregatedJobFieldData),
@@ -101,11 +87,6 @@ const Jobs: React.FC = () => {
         setSalaryChartData({
             labels: Object.keys(aggregatedAverageSalaryData),
             counts: Object.values(aggregatedAverageSalaryData)
-        });
-
-        setPrerequisitesChartData({
-            labels: Object.keys(aggregatedPrerequisitesData),
-            counts: Object.values(aggregatedPrerequisitesData)
         });
 
     }, [data]);
@@ -126,6 +107,31 @@ const Jobs: React.FC = () => {
         setShowPrerequisites(true);
     };
 
+    const handleJobFieldSelection = (selected: any) => {
+        setSelectedJobFields(selected);
+    };
+
+    const handleSubmit = () => {
+        // Filter data based on selected job fields
+        const selectedFields = selectedJobFields.map(field => field.value);
+        const filteredJobs = data.data.filter(job => selectedFields.includes(job.joblField));
+
+        // Update charts based on filtered jobs
+        const filteredJobFieldData = aggregateDataByJobField(filteredJobs);
+        const filteredAverageSalaryData = aggregateAverageSalaryByJobField(filteredJobs);
+
+        setJobFieldChartData({
+            labels: Object.keys(filteredJobFieldData),
+            counts: Object.values(filteredJobFieldData)
+        });
+
+        setSalaryChartData({
+            labels: Object.keys(filteredAverageSalaryData),
+            counts: Object.values(filteredAverageSalaryData)
+        });
+        setOptions(data.data.map(job => job.joblField).filter((value, index, self) => self.indexOf(value) === index).map(label => ({ value: label, label })));
+    };
+
     if (loading) {
         return <p>Loading...</p>;
     }
@@ -137,7 +143,7 @@ const Jobs: React.FC = () => {
     return (
         <div className="space-y-8 m-4">
             <h1 className="text-2xl font-bold text-center my-4">Job Information</h1>
-           
+
             <div className="flex flex-wrap justify-center gap-4">
                 <div className="w-full sm:w-1/2 md:w-1/3 p-2" style={{ maxHeight: '300px', maxWidth: '300px' }}>
                     {jobFieldChartData.labels.length > 0 && <JobsFieldCountChart data={jobFieldChartData} />}
@@ -146,12 +152,32 @@ const Jobs: React.FC = () => {
                     {salaryChartData.labels.length > 0 && <SalaryChart data={salaryChartData} />}
                 </div>
                 <div className="flex items-center justify-center  font-bold w-full sm:w-1/2 md:w-1/3 p-2" style={{ maxHeight: '300px', maxWidth: '300px' }}>
-                    {showPrerequisites && prerequisitesChartData.labels.length > 0 && <PrerequisitesChart data={{ labels: Object.keys(selectedJob), counts: Object.values(selectedJob) }} />}
+                    {showPrerequisites && Object.keys(selectedJob).length > 0 && <PrerequisitesChart data={{ labels: Object.keys(selectedJob), counts: Object.values(selectedJob) }} />}
                     {!showPrerequisites && <p>Select a job to view prerequisites</p>}
-               
                 </div>
             </div>
-           
+
+            <div className='w-full md:w-1/4 mx-auto'>
+                <div className="flex items-center space-x-3 ">
+                    <Select
+                        id="jobFields"
+                        name="jobFields"
+                        placeholder = "Select up to 5 Job Fields..."
+                        isMulti
+                        options={options}
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                        value={selectedJobFields}
+                        onChange={handleJobFieldSelection}
+                    />
+                    <button
+                        onClick={handleSubmit}
+                        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 rounded-r rounded-l"
+                        >
+                        Submit
+                    </button>
+                </div>
+            </div>
             <div className="w-full md:w-1/2 mx-auto">
                 <input
                     type="text"
