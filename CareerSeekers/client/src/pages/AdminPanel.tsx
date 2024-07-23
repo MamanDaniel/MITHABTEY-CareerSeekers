9ed87eed460ef2cb5eb714f19c221024acb9a022
Ben04 <benmerigen@gmail.com>
2024-07-23 19:30:38 +0300

    Convert some pages to hebrew

--- CareerSeekers/client/src/pages/AdminPanel.tsx
+++ CareerSeekers/client/src/pages/AdminPanel.tsx
@@ -14,9 +14,9 @@ export default function AdminPanel() {
     return (
         <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-3xl mx-auto">
-                <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">Hello {currentUser.username}</h1>
-                <h3 className='text-xl sm:text-2xl font-bold mt-6 text-gray-600 text-center'>This is a `Admin Panel` landing page.</h3>
-                <h3 className='text-xl sm:text-2xl font-bold mt-6 text-gray-600 text-center mb-10'>Here you can manage your users and jobs in the DataBase</h3>
+                <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">{currentUser.username}  ,שלום</h1>
+                <h3 className='text-xl sm:text-2xl font-bold mt-6 text-gray-600 text-center'>זהו דף הנחיתה של מנהלת האתר</h3>
+                <h3 className='text-xl sm:text-2xl font-bold mt-6 text-gray-600 text-center mb-10'>כאן ניתן לנהל את המידע אודות המשתמשים והמקצועות שבאתר</h3>
                 <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
                     <AdminCard 
                         title="Add new job to the database"
--- CareerSeekers/client/src/pages/DeleteJob.tsx
+++ CareerSeekers/client/src/pages/DeleteJob.tsx
@@ -4,18 +4,6 @@ import { FaTrash } from 'react-icons/fa';
 export default function DeleteJob() {
     const [jobs, setJobs] = useState([]);
     const [err, setError] = useState('');
-    // const [urls, setUrls] = useState([]);
-    // useEffect(() => {
-    //     fetch('/server/job/getURLjobs')
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             setUrls(data.jobs);
-    //         })
-    //         .catch(err => {
-    //             setError('Error fetching data');
-    //         });
-            
-    // }, [urls]);
 
     useEffect(() => {
         fetch('/server/job/getalljobnames')
@@ -51,18 +39,8 @@ export default function DeleteJob() {
  
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-6 text-center">Delete Job Page</h1>
+            <h1 className="text-3xl font-bold mb-6 text-center">מחיקת מקצוע</h1>
             {err && <p className="text-red-500 text-center mb-4">{err}</p>}
-            {/* <h2>URLs:</h2>
-            <ul>
-                {urls.map((url: any) => (
-                    <li key={url._id}>
-                        <a href={url.facebookPostUrl} target="_blank" rel="noopener noreferrer">
-                                    {url.facebookPostUrl}
-                                </a>
-                                </li>
-                ))}
-            </ul> */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {jobs.map((job: any) => (
                     <div key={job._id} className="bg-white shadow-md rounded-lg p-4 flex justify-between items-center">
