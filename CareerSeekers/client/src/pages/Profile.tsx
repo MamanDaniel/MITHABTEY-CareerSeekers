2f7e1f817b5fd714b08acafb8d9e50a87d674b5c
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2024-08-28 16:21:13 +0300

    add user traits to profile page

--- CareerSeekers/client/src/pages/Profile.tsx
+++ CareerSeekers/client/src/pages/Profile.tsx
@@ -1,23 +1,58 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { FaInfoCircle } from 'react-icons/fa';
 import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
-  signOutUserFailure,
-  signOutUserSuccess,
-  signOutUserStart,
 } from '../redux/user/userSlice';
-import { useDispatch } from 'react-redux';
-import { FaInfoCircle } from 'react-icons/fa';
+import { jobFields } from '../components/jobs/jobFieldMapping'; // Import the jobFields mapping
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state: any) => state.user);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [avatar, setAvatar] = useState(currentUser.avatar); // Store avatar
+  const [traits, setTraits] = useState({});
   const dispatch = useDispatch();
+
+  // Fetch user traits when the component mounts
+  useEffect(() => {
+    const fetchTraits = async () => {
+      try {
+        const res = await fetch(`/server/user/getUserTraits/`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id: currentUser._id }),
+        });
+        const data = await res.json();
+        if (res.ok) {
+          // Adjust the trait names before setting the state
+          const adjustedTraits: { [key: string]: any } = {};
+          for (const [trait, value] of Object.entries(data)) {
+            if (trait === 'ArtsAndEntertainment') {
+              adjustedTraits['Arts And Entertainment'] = value;
+            } else if (trait === 'GeneralCulture') {
+              adjustedTraits['General Culture'] = value;
+            } else {
+              adjustedTraits[trait] = value;
+            }
+          }
+          setTraits(adjustedTraits);
+        } else {
+          console.error('Failed to fetch traits:', data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching traits:', error);
+      }
+    };
+
+    fetchTraits();
+  }, [currentUser._id]);
 
   const handleChange = (e: any) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -25,30 +60,26 @@ export default function Profile() {
 
   const handleFileChange = (e: any) => {
     const file = e.target.files[0];
-    
-    // Check if the file is selected
+
     if (file) {
-      // Check file type
       const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
       if (!allowedTypes.includes(file.type)) {
         alert('נא לבחור קובץ בפורמט PNG, JPG או JPEG.');
         return;
       }
 
-      // Check file size
       const maxSize = 5 * 1024 * 1024; // 5MB in bytes
       if (file.size > maxSize) {
         alert('גודל הקובץ לא יכול לעלות על 5MB.');
         return;
       }
 
-      // If both checks pass, read the file
       const reader = new FileReader();
       reader.onloadend = () => {
         setAvatar(reader.result); // Update avatar state with Base64
         setFormData({ ...formData, avatar: reader.result }); // Include Base64 in formData
       };
-      
+
       reader.readAsDataURL(file);
     }
   };
@@ -76,20 +107,7 @@ export default function Profile() {
     }
   };
 
-  const handleSignOut = async () => {
-    try {
-      dispatch(signOutUserStart());
-      const res = await fetch('/server/auth/signout');
-      const data = await res.json();
-      if (data.success === false) {
-        dispatch(signOutUserFailure(data.message));
-        return;
-      }
-      dispatch(signOutUserSuccess(data));
-    } catch (error) {
-      dispatch(signOutUserFailure(error.message));
-    }
-  };
+
 
   const formattedJoinDate = new Date(currentUser.createdAt).toLocaleDateString('he-IL');
 
@@ -108,70 +126,87 @@ export default function Profile() {
       </div>
 
       <p className='text-lg text-gray-700 text-center mb-8'>
-        בעמוד זה ניתן לעדכן מידע אישי ופרטים נוספים 
+        בעמוד זה ניתן לעדכן מידע אישי ופרטים נוספים
       </p>
 
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4 mb-8'>
-        {['username', 'email', 'password'].map((field, index) => (
-          <div key={index} className='flex items-center'>
-            <div className='w-11/12'>
-              <input
-                type={field === 'password' ? 'password' : field}
-                placeholder={field === 'username' ? 'שם משתמש' : field === 'email' ? 'דואר אלקטרוני' : 'סיסמה'}
-                defaultValue={currentUser[field]}
-                id={field}
-                onChange={handleChange}
-                className='border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-full'
-              />
-            </div>
-            <div className='w-1/12 flex justify-center'>
-              <div className='relative group'>
-                <FaInfoCircle className='text-blue-500' />
-                <div className='hidden group-hover:block absolute -top-8 right-0 bg-gray-800 text-white text-xs rounded py-1 px-2 text-right'>
-                  {field === 'username' ? 'שם משתמש' : field === 'email' ? ' דואר אלקטרוני' : 'סיסמה'}
+      <div className='flex flex-col md:flex-row justify-center items-start gap-10 mb-8'>
+        <div className='flex flex-col justify-between w-full md:w-1/2'>
+          <div className='bg-blue-50 p-6 rounded-lg shadow-md w-full '>
+            <h3 className='text-2xl font-semibold text-blue-600 mb-4'>
+              התכונות שלך
+            </h3>
+            <ul className='space-y-2'>
+              {Object.entries(traits).map(([trait, value]) => (
+                <li key={trait} className='flex justify-between items-center text-lg text-gray-700'>
+                  <span>{jobFields[trait]?.hebrew || trait}:</span> <span>{value as React.ReactNode}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='flex justify-center items-center p-4 bg-gray-100 rounded-lg shadow-md mt-4 '>
+            <Link to='/RamakQuestionnaire' className='text-blue-600 hover:underline'>
+              עדכון תכונות בשאלון RAMAK
+            </Link>
+          </div>
+        </div>
+
+        <div className='flex flex-col justify-between w-full md:w-1/2'>
+          <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+            {['username', 'email', 'password'].map((field, index) => (
+              <div key={index} className='flex items-center'>
+                <div className='w-11/12'>
+                  <input
+                    type={field === 'password' ? 'password' : field}
+                    placeholder={field === 'username' ? 'שם משתמש' : field === 'email' ? 'דואר אלקטרוני' : 'סיסמה'}
+                    defaultValue={currentUser[field]}
+                    id={field}
+                    onChange={handleChange}
+                    className='border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-full'
+                  />
+                </div>
+                <div className='w-1/12 flex justify-center'>
+                  <div className='relative group'>
+                    <FaInfoCircle className='text-blue-500' />
+                    <div className='hidden group-hover:block absolute -top-8 right-0 bg-gray-800 text-white text-xs rounded py-1 px-2 text-right'>
+                      {field === 'username' ? 'שם משתמש' : field === 'email' ? ' דואר אלקטרוני' : 'סיסמה'}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+
+            {/* Image Upload Section */}
+            <div className='flex items-center'>
+              <div className='w-11/12'>
+                <input
+                  type="file"
+                  accept="image/png, image/jpg, image/jpeg"
+                  onChange={handleFileChange}
+                  className='border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-full'
+                />
+              </div>
+              <div className='w-1/12 flex justify-center'>
+                <div className='relative group'>
+                  <FaInfoCircle className='text-blue-500' />
+                  <div className='hidden group-hover:block absolute -top-8 right-0 bg-gray-800 text-white text-xs rounded py-1 px-2 text-right'>
+                    העלאת תמונה
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+            <div className='-mt-2'><span className='text-sm text-gray-600 text-center mt'>בחר תמונה (תמיכה בפורמטים PNG, JPG, JPEG)</span></div>
 
-        {/* Image Upload Section */}
-        <div className='flex items-center'>
-          <div className='w-11/12'>
-            <input
-              type="file"
-              accept="image/png, image/jpg, image/jpeg"
-              onChange={handleFileChange}
-              className='border p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-full'
-            />
-          </div>
-          <div className='w-1/12 flex justify-center'>
-            <div className='relative group'>
-              <FaInfoCircle className='text-blue-500' />
-              <div className='hidden group-hover:block absolute -top-8 right-0 bg-gray-800 text-white text-xs rounded py-1 px-2 text-right'>
-                העלאת תמונה
-              </div>
-            </div>
-          </div>
+            <button disabled={loading} className='bg-blue-600 text-white rounded-lg p-3 uppercase hover:bg-blue-700 transition disabled:opacity-70 mt-3'>
+              עדכון פרטים
+            </button>
+            {updateSuccess && <p className='text-green-600 text-center'>עדכון הצליח!</p>}
+          </form>
+
+          {error && <p className='text-red-600 text-center'>{error}</p>}
         </div>
-        <div className='-mt-3'><span className='text-sm text-gray-600 text-center '>בחר תמונה (תמיכה בפורמטים PNG, JPG, JPEG)</span></div>
-
-        <button disabled={loading} className='bg-blue-600 text-white rounded-lg p-3 uppercase hover:bg-blue-700 transition disabled:opacity-70'>
-          {loading ? <div className='loader'></div> : 'עדכן פרופיל'}
-        </button>
-      </form>
-
-      <div className="flex justify-between items-center">
-        <Link to='/RamakQuestionnaire' className='text-blue-700 hover:underline'>
-          עדכון תכונות בשאלון RAMAK
-        </Link>
-        <span onClick={handleSignOut} className='text-red-700 hover:underline cursor-pointer'>
-          התנתקות
-        </span>
       </div>
 
-      {error && <p className='text-red-700 mt-5'>{error}</p>}
-      {updateSuccess && <p className='text-green-700 mt-5 text-center'>המשתמש עודכן בהצלחה</p>}
     </div>
   );
 }
